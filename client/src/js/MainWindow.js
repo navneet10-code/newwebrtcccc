10380ce10aaf66918888bbfb23608d78a8a571e6
navneet10-code <54025358+navneet10-code@users.noreply.github.com>
2019-09-13 12:55:16 +0530

    Update MainWindow.js

--- client/src/js/MainWindow.js
+++ client/src/js/MainWindow.js
@@ -29,25 +29,7 @@ class MainWindow extends Component {
     console.log(`${clientId}`);
     document.title = `${clientId} - VideoCall`;
     
-    
-    
-    var pg = require('pg');
- console.log('inside');
-app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM user', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('pages/db', {results: result.rows} ); }
-    });
-  });
-});
-    
-    
-    
-    
+   
     
     
     return (
