5aaa271203eddc15ff63e27bb933fb7a56602467
navneet10-code <54025358+navneet10-code@users.noreply.github.com>
2019-09-13 12:44:14 +0530

    Update index.js

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -2,7 +2,7 @@ var pg = require('pg');
  console.log('inside');
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
+    client.query('SELECT * FROM user', function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
