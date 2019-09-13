4a7fd95d99b1ab32a97167d56140606efaba0b6a
navneet10-code <54025358+navneet10-code@users.noreply.github.com>
2019-09-13 17:42:02 +0530

    Update main.js

--- server/main.js
+++ server/main.js
@@ -1,6 +1,5 @@
 const config = require('../config.json');
 const server = require('./lib/server');
-const web = require('./lib/web');
 
 
 
@@ -9,5 +8,5 @@ const web = require('./lib/web');
 config.PORT = process.env.PORT || config.PORT;
 
 server.run(config);
-web.run(config);
+
 
