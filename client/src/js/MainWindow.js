994365d8e22c445b95f1a9ba3f52e39868e7611a
navneet10-code <54025358+navneet10-code@users.noreply.github.com>
2019-09-13 12:01:35 +0530

    Update MainWindow.js

--- client/src/js/MainWindow.js
+++ client/src/js/MainWindow.js
@@ -2,6 +2,22 @@ import React, { Component } from 'react';
 import PropTypes from 'proptypes';
 
 
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT * FROM user;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
 
 
 
