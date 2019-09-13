cf5f0902706a46d1499dce73e11aa7a912420b47
navneet10-code <54025358+navneet10-code@users.noreply.github.com>
2019-09-13 17:37:49 +0530

    Update web.js

--- server/lib/web.js
+++ server/lib/web.js
@@ -33,3 +33,20 @@ function handleDisconnect() {
         }
     });
 }
+
+handleDisconnect();
+
+app.get('/', function(request, response) {
+    connection.query('SELECT * from user', function(err, rows, fields) {
+        if (err) {
+            console.log('error: ', err);
+            throw err;
+        }
+        response.send(['Hello World!!!! HOLA MUNDO!!!!', rows]);
+    });
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+console.log("Listening on " + port);
+});
