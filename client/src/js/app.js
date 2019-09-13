b6bd7925a0b5859ef91d9f1a0c8d2123deb08bcd
navneet10-code <54025358+navneet10-code@users.noreply.github.com>
2019-09-13 12:10:34 +0530

    Update app.js

--- client/src/js/app.js
+++ client/src/js/app.js
@@ -86,29 +86,6 @@ endCall(isStarter) {
     });
   }
  
-  database() {
-  
-  const { Client } = require('pg');
-
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-});
-
-client.connect();
-
-client.query('SELECT * FROM user;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-});
-
-  
-  }
-  
-  
   
   
   
