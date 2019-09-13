bf5e3051d5f669508397bba83aa5167ceb9c05ff
navneet10-code <54025358+navneet10-code@users.noreply.github.com>
2019-09-13 13:55:27 +0530

    Create web.js

---
+++ server/web.js
@@ -0,0 +1,14 @@
+var mysql = require('mysql'); 
+var con = mysql.createConnection({ 
+host : 'sql12.freemysqlhosting.net',
+database : 'sql12304794',
+user : 'sql12304794',
+password : 'PLSEEGHnWv', 
+}); 
+con.connect(function(err) { 
+if (err) throw err; 
+con.query("SELECT * FROM user", function (err, result) { 
+if (err) throw err; 
+console.log(result); 
+}); 
+});
