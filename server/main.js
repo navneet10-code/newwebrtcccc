76c72fdb18cdb284d7c39fce192067499841cb11
navneet10-code <54025358+navneet10-code@users.noreply.github.com>
2019-09-13 11:51:36 +0530

    Update main.js

--- server/main.js
+++ server/main.js
@@ -4,24 +4,3 @@ const server = require('./lib/server');
 config.PORT = process.env.PORT || config.PORT;
 
 server.run(config);
-
-
-
-
-
-const { Client } = require('pg');
-
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-});
-
-client.connect();
-
-client.query('SELECT * FROM user;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-});
